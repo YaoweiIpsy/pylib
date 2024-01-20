6d8ee42769a9fab5d3d6f05b37340b3273912b71
Yaowei <yaowei@ipsy.com>
2024-01-20 00:28:17 -0800

    a test utils

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='pylib',
+    version='0.1',
+    packages=['utils'],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
